58c8f08f5554c098807d63c85a5fd6313936b3c5
yeasinrafee <mdyeasinrafi7@gmail.com>
2025-05-11 13:18:54 +0600

    Doctor schedule

---
+++ src/app/(withDashBoardLayout)/dashboard/doctor/schedules/components/DoctorScheduleModal.tsx
@@ -0,0 +1,95 @@
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+import dayjs from 'dayjs';
+import { useState } from 'react';
+import MultipleSelectFieldChip from './MultipleSelectFieldChip';
+import { Stack } from '@mui/material';
+import LoadingButton from '@mui/lab/LoadingButton';
+import { useCreateDoctorScheduleMutation } from '@/redux/api/doctorScheduleApi';
+import { useGetAllSchedulesQuery } from '@/redux/api/schedule.Api';
+import HCModal from '@/components/Shared/HCModal/HCModal';
+
+type TProps = {
+  open: boolean;
+  setOpen: React.Dispatch<React.SetStateAction<boolean>>;
+};
+
+const DoctorScheduleModal = ({ open, setOpen }: TProps) => {
+  const [selectedDate, setSelectedDate] = useState(
+    dayjs(new Date()).toISOString()
+  );
+
+  const [selectedScheduleIds, setSelectedScheduleIds] = useState<string[]>([]);
+
+  const query: Record<string, any> = {};
+
+  if (!!selectedDate) {
+    query['startDateTime'] = dayjs(selectedDate)
+      .hour(0)
+      .minute(0)
+      .millisecond(0)
+      .toISOString();
+    query['endDateTime'] = dayjs(selectedDate)
+      .hour(23)
+      .minute(59)
+      .millisecond(999)
+      .toISOString();
+  }
+
+  const { data } = useGetAllSchedulesQuery(query);
+  const schedules = data?.schedules;
+  console.log(schedules);
+
+  const [createDoctorSchedule, { isLoading }] =
+    useCreateDoctorScheduleMutation();
+
+  console.log(selectedScheduleIds);
+
+  const onSubmit = async () => {
+    try {
+      const res = await createDoctorSchedule({
+        scheduleIds: selectedScheduleIds,
+      });
+      console.log(res);
+      setOpen(false);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  console.log(selectedScheduleIds);
+  return (
+    <HCModal open={open} setOpen={setOpen} title='Create Doctor Schedule'>
+      <Stack direction={'column'} gap={2}>
+        <LocalizationProvider dateAdapter={AdapterDayjs}>
+          <DatePicker
+            label='Controlled picker'
+            value={dayjs(selectedDate)}
+            onChange={(newValue) =>
+              setSelectedDate(dayjs(newValue).toISOString())
+            }
+            sx={{ width: '100%' }}
+          />
+        </LocalizationProvider>
+        <MultipleSelectFieldChip
+          schedules={schedules}
+          selectedScheduleIds={selectedScheduleIds}
+          setSelectedScheduleIds={setSelectedScheduleIds}
+        />
+
+        <LoadingButton
+          size='small'
+          onClick={onSubmit}
+          loading={isLoading}
+          loadingIndicator='Submitting...'
+          variant='contained'
+        >
+          <span>Submit</span>
+        </LoadingButton>
+      </Stack>
+    </HCModal>
+  );
+};
+
+export default DoctorScheduleModal;
---
+++ src/app/(withDashBoardLayout)/dashboard/doctor/schedules/page.tsx
@@ -0,0 +1,123 @@
+'use client';
+import { Box, Button, IconButton, Pagination } from '@mui/material';
+import DoctorScheduleModal from './components/DoctorScheduleModal';
+import { useEffect, useState } from 'react';
+import { DataGrid, GridColDef } from '@mui/x-data-grid';
+import DeleteIcon from '@mui/icons-material/Delete';
+import { dateFormatter } from '@/utils/dateFormatter';
+import dayjs from 'dayjs';
+import { useGetAllDoctorSchedulesQuery } from '@/redux/api/doctorScheduleApi';
+import AddIcon from '@mui/icons-material/Add';
+import { TSchedule } from '@/types/schedule';
+
+const DoctorSchedulesPage = () => {
+  const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
+
+  const query: Record<string, any> = {};
+
+  const [page, setPage] = useState(1);
+  const [limit, setLimit] = useState(3);
+
+  query['page'] = page;
+  query['limit'] = limit;
+
+  const [allSchedule, setAllSchedule] = useState<any>([]);
+  const { data, isLoading } = useGetAllDoctorSchedulesQuery({ ...query });
+
+  const schedules = data?.doctorSchedules?.data;
+  const meta = data?.doctorSchedules?.meta;
+
+  let pageCount: number;
+
+  if (meta?.total) {
+    pageCount = Math.ceil(meta.total / limit);
+  }
+
+  const handleChange = (event: React.ChangeEvent<unknown>, value: number) => {
+    setPage(value);
+  };
+
+  useEffect(() => {
+    const updateData = schedules?.map((schedule: TSchedule, index: number) => {
+      return {
+        sl: index + 1,
+        id: schedule?.id,
+        startDate: dateFormatter(schedule?.startDateTime),
+        startTime: dayjs(schedule?.startDateTime).format('hh:mm a'),
+        endTime: dayjs(schedule?.endDateTime).format('hh:mm a'),
+      };
+    });
+    setAllSchedule(updateData);
+  }, [schedules]);
+
+  const columns: GridColDef[] = [
+    { field: 'sl', headerName: 'SL' },
+    { field: 'startDate', headerName: 'Date', flex: 1 },
+    { field: 'startTime', headerName: 'Start Time', flex: 1 },
+    { field: 'endTime', headerName: 'End Time', flex: 1 },
+    {
+      field: 'action',
+      headerName: 'Action',
+      flex: 1,
+      headerAlign: 'center',
+      align: 'center',
+      renderCell: ({ row }) => {
+        return (
+          <IconButton aria-label='delete'>
+            <DeleteIcon sx={{ color: 'red' }} />
+          </IconButton>
+        );
+      },
+    },
+  ];
+
+  return (
+    <Box>
+      <Button
+        onClick={() => setIsModalOpen(true)}
+        endIcon={<AddIcon />}
+        sx={{ mt: 3.5 }}
+      >
+        Create Doctor Schedule
+      </Button>
+      <DoctorScheduleModal open={isModalOpen} setOpen={setIsModalOpen} />
+      <Box sx={{ mb: 5 }}></Box>
+
+      <Box>
+        {!isLoading ? (
+          <Box my={2}>
+            <DataGrid
+              rows={allSchedule ?? []}
+              columns={columns}
+              hideFooterPagination
+              slots={{
+                footer: () => {
+                  return (
+                    <Box
+                      sx={{
+                        mb: 2,
+                        display: 'flex',
+                        justifyContent: 'center',
+                      }}
+                    >
+                      <Pagination
+                        color='primary'
+                        count={pageCount}
+                        page={page}
+                        onChange={handleChange}
+                      />
+                    </Box>
+                  );
+                },
+              }}
+            />
+          </Box>
+        ) : (
+          <h1>Loading.....</h1>
+        )}
+      </Box>
+    </Box>
+  );
+};
+
+export default DoctorSchedulesPage;
